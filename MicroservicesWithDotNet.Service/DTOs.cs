4b3226e4ce5676ac04b03c7692a0386888aec245
Kateryna Bilinska <katerynabilinska11@gmail.com>
2024-02-06 22:45:00 +0200

    Created first microservices, added some dto records and few crud operations

---
+++ MicroservicesWithDotNet.Service/DTOs.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroservicesWithDotNet.Service.DTOs
+{
+    public record ItemDTO(Guid Id, string Name, string Description, decimal Price);
+
+    public record CreateItemDTO([Required]string Name, string Description, [Range(0, 1000)]decimal Price);
+
+    public record UpdateItemDTO([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
+}
